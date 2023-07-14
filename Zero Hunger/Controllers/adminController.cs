0e18a1498f1d4570d7f57e926220b628ae01eb92
Monkir Chowdhury <monkirchowdhury@gmail.com>
2023-07-14 23:21:43 +0600

    Admin Auth, add employee

---
+++ Zero Hunger/Controllers/adminController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zero_Hunger.Auth;
+using Zero_Hunger.DB;
+using Zero_Hunger.Models;
+
+namespace Zero_Hunger.Controllers
+{
+    public class adminController : Controller
+    {
+        // GET: employee
+        [adminAccess]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult login(loginDTO obj)
+        {
+            var db = new zero_hungerEntities();
+            var user = (from u in db.admins
+                        where 
+                            u.username.Equals(obj.username) &&
+                            u.password.Equals(obj.password)
+                        select u).SingleOrDefault();
+            if (user != null)
+            {
+                Session["user"] = user.username;
+                Session["id"] = user.id;
+                Session["type"] = "admin";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpGet]
+        [adminAccess]
+        public ActionResult addemployee() 
+        {
+            return View();
+        }
+        [HttpPost]
+        [adminAccess]
+        public ActionResult addemployee(addemployeeDTO empModel) 
+        {
+            if(ModelState.IsValid)
+            {
+                var db = new zero_hungerEntities();
+                db.employees.Add(convert(empModel));
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(empModel);
+        }
+        employee convert(addemployeeDTO empDTO)
+        {
+            employee emp = new employee()
+            {
+                name = empDTO.name,
+                email = empDTO.email,
+                phone = empDTO.phone,
+                address = empDTO.address,
+                dob = empDTO.dob,
+                username = empDTO.username,
+                password = empDTO.password,
+                admin_id = (int?)Session["id"]
+            };
+            return emp;
+        }
+    }
+}
