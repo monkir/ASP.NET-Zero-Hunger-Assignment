aff16dd5ecb15d042e0c27dde832c9ad0ce5ee9d
Monkir Chowdhury <monkirchowdhury@gmail.com>
2023-07-20 22:45:08 +0600

    L8

---
+++ zero-hunger-mid-assignment/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace zero_hunger_mid_assignment
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ zero-hunger-mid-assignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="zero_hunger_mid_assignment.MvcApplication" Language="C#" %>
